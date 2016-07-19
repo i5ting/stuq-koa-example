81ec1b996fb166f991046a8a40e075087a96e9d6
i5ting <i5ting@126.com>
2016-07-19 19:03:28 +0800

    lazycommmit, please ignore

---
+++ koa-getting-start/2.x/app.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// logger
+
+app.use((ctx, next) => {
+  const start = new Date();
+  return next().then(() => {
+    const ms = new Date() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  });
+});
+
+// response
+app.use(ctx => {
+  ctx.body = 'Hello Koa';
+});
+
+app.listen(3000);
